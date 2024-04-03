a4a7b95786afc5d77addc232a12d11851c0a3be6
Phuoc Tai <87972973+phuoctai1644@users.noreply.github.com>
2024-04-03 22:01:56 +0700

    feat: handle serializers

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, include
 from rest_framework import routers
+from .views import MovieViewSet, RatingViewSet
 
 router = routers.DefaultRouter()
+router.register('movies', MovieViewSet)
+router.register('ratings', RatingViewSet)
 
 urlpatterns = [
     path('', include(router.urls))
