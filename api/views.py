b946212c38af36fe8dadc19a2d1c6aec8bbf3c01
Phuoc Tai <87972973+phuoctai1644@users.noreply.github.com>
2024-04-04 21:52:52 +0700

    feat: handle rate_movie api

--- api/views.py
+++ api/views.py
@@ -1,13 +1,18 @@
-from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Movie, Rating
 from .serializers import MovieSerializer, RatingSerializer
-
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import action
 
 class MovieViewSet(viewsets.ModelViewSet):
     serializer_class = MovieSerializer
     queryset = Movie.objects.all()
 
+    @action(detail=True, methods=['POST'])
+    def rate_movie(self, request, pk=None):
+        response = {'message': 'its working'}
+        return Response(response, status=status.HTTP_200_OK)
 
 class RatingViewSet(viewsets.ModelViewSet):
     queryset = Rating.objects.all()
