8d9bd2211bb22f0e0b8c29e86b628aa388e796b5
Phuoc Tai <87972973+phuoctai1644@users.noreply.github.com>
2024-05-19 23:30:42 +0700

    feat: handle change-avatar api

--- api/views.py
+++ api/views.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import AllowAny
+from rest_framework.parsers import MultiPartParser
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -12,6 +13,7 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (AllowAny, )
+    parser_classes = (MultiPartParser,)
 
     @action(detail=False, methods=['GET'])
     def profile(self, request):
@@ -52,6 +54,23 @@ class UserViewSet(viewsets.ModelViewSet):
         except:
             response = {'message': 'Cannot find this user!'}
             return Response(response, status=status.HTTP_400_BAD_REQUEST)
+        
+    
+    @action(detail=True, methods=['PUT'])
+    def change_avatar(self, request, pk=None):
+        try:
+            user = User.objects.get(id=pk)
+        except User.DoesNotExist:
+            return Response({'message': 'Cannot find this user!'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        if 'avatar' not in request.FILES:
+            return Response({'message': 'No avatar file provided!'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        avatar = request.FILES['avatar']
+        user.avatar = avatar
+        user.save()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 
 
 class MovieViewSet(viewsets.ModelViewSet):
